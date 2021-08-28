ee8556eba1b3e885457ce13b34d67d50a2ea064f
JacobHocker <jacobdouglas06@gmail.com>
2021-08-28 11:22:13 -0500

    added pictures and other updates

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,27 +2,35 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
+    const linkStyles = {
+        color: "white",
+        padding: "40px",
+        width: "100px",
+        margin: "0 6px 6px",
+        textDecoration: "none",
+        background: "blue",
+    }
     return(
         <div className="navbar">
-            <NavLink to="/">
+            <NavLink to="/" style={linkStyles}>
                 Home
             </NavLink>
-            <NavLink to="/quotes">
+            <NavLink to="/quotes"  style={linkStyles}>
                 Quotes
             </NavLink>
-            <NavLink to="/did-you-know-?">
+            <NavLink to="/did-you-know-?"  style={linkStyles}>
                 Did You Know?
             </NavLink>
-            <NavLink to="/submit-a-quote">
+            <NavLink to="/submit-a-quote"  style={linkStyles}>
                 Submit A Quote
             </NavLink>
-            <NavLink to="/submit-a-fact">
+            <NavLink to="/submit-a-fact"  style={linkStyles}>
                 Submit A Fact
             </NavLink>
-            <NavLink to="/edit-a-quote">
+            <NavLink to="/edit-a-quote"  style={linkStyles}>
                 Edit A Quote
             </NavLink>
-            <NavLink to="/contact-me">
+            <NavLink to="/contact-me"  style={linkStyles}>
                 Contact Me
             </NavLink>
         </div>
