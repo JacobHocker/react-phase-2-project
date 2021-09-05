e6ffbc97c0ab496116eb8e19083f08c846cae1ea
JacobHocker <jacobdouglas06@gmail.com>
2021-09-05 14:28:33 -0500

    added the active styles to navbar and links to icons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,22 +7,27 @@ function NavBar() {
         margin: "0 6px 6px",
         textDecoration: "none",
         background: "black",
-        fontSize: "1.5rem",
+        fontSize: "2.0rem",
+    }
+
+    const activeStyles = {
+        color: "white",
+        background: "linear-gradient(90deg, black, gold)"
     }
 
    
     return(
         <div className="navbar">
-            <NavLink to="/" exact style={linkStyles}>
+            <NavLink to="/" exact style={linkStyles} activeStyle={activeStyles}>
                 Home
             </NavLink>
-            <NavLink to="/quotes" exact  style={linkStyles}>
+            <NavLink to="/quotes" exact  style={linkStyles} activeStyle={activeStyles}>
                 Quotes
             </NavLink>
-            <NavLink to="/did-you-know" exact  style={linkStyles}>
+            <NavLink to="/did-you-know" exact  style={linkStyles} activeStyle={activeStyles}>
                 Did You Know?
             </NavLink>
-            <NavLink to="/contact-me" exact  style={linkStyles}>
+            <NavLink to="/contact-me" exact  style={linkStyles} activeStyle={activeStyles}>
                 Contact Me
             </NavLink>
         </div>
